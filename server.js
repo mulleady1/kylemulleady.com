689be5fe91281a61c950644dff6bf067dc1cd9e9
Kyle Mulleady <kyle.mulleady@gmail.com>
2016-07-13 10:14:08 -0700

    Keep ExtractTextPlugin in webpack config

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var config = require('./webpack.config');
 config.entry.unshift('webpack/hot/only-dev-server');
 config.entry.unshift('webpack-dev-server/client?http://localhost:5001');
 config.module.loaders[0].loaders.unshift('react-hot');
-config.plugins.unshift(new webpack.HotModuleReplacementPlugin());
+config.module.loaders[1].loader = 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader!sass-loader?outputStyle=expanded';
+config.plugins[0] = new webpack.HotModuleReplacementPlugin();
 
 const HOST = '0.0.0.0';
 const PORT = 5001;
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const autoprefixer = require('autoprefixer');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
 	devtool: 'eval',
@@ -21,11 +22,12 @@ module.exports = {
 			},
 			{
 				test: /\.(css|scss)$/,
-				loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader!sass-loader?outputStyle=expanded'
+				loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader!sass-loader?outputStyle=expanded')
 			}
 		]
 	},
 	plugins: [
+		new ExtractTextPlugin('css/styles.css'),
 		new webpack.DefinePlugin({
 			'process.env.NODE_ENV': '"development"'
 		})
