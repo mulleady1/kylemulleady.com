f61238de72684109a37e03be551e27dd897ab9fb
Kyle Mulleady <kyle.mulleady@gmail.com>
2016-07-19 21:19:46 -0700

    Switch to browserHistory

--- src/history/index.js
+++ src/history/index.js
@@ -1,11 +1,3 @@
-import { useRouterHistory } from 'react-router';
-import { createHashHistory } from 'history';
-import store from '../store';
+import { browserHistory } from 'react-router';
 
-const history = useRouterHistory(createHashHistory)({ queryKey: false });
-
-history.listen((location) => {
-	// Do stuff as needed.
-});
-
-export default history;
+export default browserHistory;
